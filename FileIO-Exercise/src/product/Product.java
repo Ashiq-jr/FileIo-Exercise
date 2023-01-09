2971273d520a45b0e30b0c87b931f1cddeb1eb82
ashiq <ashiq@AshiqJr>
2023-01-09 19:45:42 +0530

    "#1 Designed the Basic Structure of the Product Class."

--- FileIO-Exercise/src/product/Product.java
+++ FileIO-Exercise/src/product/Product.java
@@ -2,8 +2,45 @@ package product;
 
 
 public class Product {
-
+		
+	String id;
+	String name;
+	String category;
+	String unitPrice;
+	String taxSlab;
+	String status;
+	
+	public Product(String id, String name, String category, String unitPrice, String taxSlab, String status) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.category = category;
+		this.unitPrice = unitPrice;
+		this.taxSlab = taxSlab;
+		this.status = status;
+	}
 
 	
+	public String getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public String getUnitPrice() {
+		return unitPrice;
+	}	
+	public String getTaxSlab() {
+		return taxSlab;
+	}
+	public String getStatus() {
+		return status;
+	}
+
+
+
 
 }
